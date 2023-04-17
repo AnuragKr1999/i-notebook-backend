2c918cd332f9a9085cddcbbaa3e4bcc87fa0489c
Anurag Kumar <anuragkr898@gmail.com>
2023-04-17 19:49:36 +0530

    modified index.js

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ app.use('/api/notes', require('./routes/notes'))
 
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`i-Notebook listening on port ${port}`)
 })
